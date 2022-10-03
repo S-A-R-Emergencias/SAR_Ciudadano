3e26f46769299e63551e0a3f45504f940a11439c
FernandoTFW <90214921+FernandoTFW@users.noreply.github.com>
2022-10-03 17:37:35 -0400

    Revert "UI Sign-In,Create Acount,Profile pull request"

--- sar_ciudadano/lib/main.dart
+++ sar_ciudadano/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sar_ciudadano/constans.dart';
-import 'package:sar_ciudadano/home/screens/sing_in_screen.dart';
-import 'package:sar_ciudadano/home/screens/register_screen.dart';
-import 'package:sar_ciudadano/home/screens/profile.dart';
 import 'package:sar_ciudadano/src/reports/report_form.dart';
 
 void main() => runApp(const MyApp());
@@ -16,14 +12,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
         textTheme:
-            Theme.of(context).textTheme.apply(bodyColor: kSecondaryColor),
+            Theme.of(context).textTheme.apply(bodyColor: Colors.white),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const ProfileScreen(),
+      home: const FormScreen(),
     );
   }
 }
-
